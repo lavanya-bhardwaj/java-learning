c4dab4974863c081a97485fd9b79155fdb60b0f7
lavanya-bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-07-01 21:23:26 +0530

    did basic arithmetic operators. borrrinnngggg

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperators{
+    public static void main(String[] args){
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+
+
+        System.out.println(z);
+    }
+}
