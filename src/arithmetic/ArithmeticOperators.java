de002e024268e8ee1bea4636b831d928801cccfa
lavanya-bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-07-01 21:32:54 +0530

    augmented assignment operators... uh already knew that but cool

---
+++ src/arithmetic/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+package arithmetic;
+
+public class ArithmeticOperators{
+    public static void main(String[] args){
+
+        // Augmented Assignment Operators
+
+        int x = 10;
+        int y = 3;
+
+        //x+=y;
+        //x-=y;
+        //x*=y;
+        //x/=y;
+        x%=y;
+
+        System.out.println(x);
+
+    }
+}
