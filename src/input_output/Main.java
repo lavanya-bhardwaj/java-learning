1e3e7b3224f00855975cf23bc7fd4a7cf78a00ac
lavanya-bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-07-01 21:01:26 +0530

    learnt user input and calculated area

--- src/input_output/Main.java
+++ src/input_output/Main.java
@@ -3,45 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        /*
+        //Calculate the area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
 
-        System.out.print("enter your age: ");
-        int age = scanner.nextInt();
+        area = width * height;
 
-        System.out.print("What is your CGPA? ");
-        double cgpa = scanner.nextDouble();
-
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-        System.out.println("hello " + name + "!");
-        System.out.println("You are "+age+ " years old");
-        System.out.println("Your CGPA is " + cgpa);
-
-        if(isStudent){
-            System.out.println("You are enrolled as a student! hurray?");
-        }
-        else{
-            System.out.println("You are NOT enrolled...");
-        }*/
-
-
-        //COMMON ISSUES
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your age:");
-        int age = scanner.nextInt();
-        scanner.nextLine(); //to clear the buffer
-
-        System.out.print("Enter your fav colour: ");
-        String color = scanner.nextLine();
-
-        System.out.println("You are " + age + " years old");
-        System.out.println("You like the colour " + color);
+        System.out.println("the area is: " + area + "cm^2");
 
         scanner.close();
     }
