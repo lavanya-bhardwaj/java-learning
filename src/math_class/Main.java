ebb0a74bb9197735e3391f60e75be91517e249e3
lavanya-bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-07-04 00:51:45 +0530

    learnt a few uses of Math

---
+++ src/math_class/Main.java
@@ -0,0 +1,21 @@
+package math_class;
+
+public class Main {
+    public static void main(String[] args){
+
+        //System.out.println(Math.PI);
+        //System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2,5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);    //round up
+        result = Math.floor(3.99);   //round down
+        result = Math.max(10,20);
+
+        System.out.println(result);
+    }
+}
