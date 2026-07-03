8e68d8032c8dfd330176542efb6f4e099dd480d8
lavanya-bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-07-04 00:58:27 +0530

    found the hypotenuse

--- src/math_class/Main.java
+++ src/math_class/Main.java
@@ -1,21 +1,26 @@
 package math_class;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        //System.out.println(Math.PI);
-        //System.out.println(Math.E);
+        // HYPOTENUSE c = Math.sqrt(a^2 + b^2)
 
-        double result;
+        Scanner scanner = new Scanner(System.in);
 
-        result = Math.pow(2,5);
-        result = Math.abs(-5);
-        result = Math.sqrt(9);
-        result = Math.round(3.14);
-        result = Math.ceil(3.14);    //round up
-        result = Math.floor(3.99);   //round down
-        result = Math.max(10,20);
+        double a;
+        double b;
+        double c;
 
-        System.out.println(result);
+        System.out.print("Enter the length of side A: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter the length of side B: ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        System.out.println("The hypotenuse (side c) is: " + c + " cm");
+
+        scanner.close();
     }
 }
